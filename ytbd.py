f87a01dec48b626d8a0f2639cb3bc79c0e496580
Alpacabet <49300674+Eric-Busch0@users.noreply.github.com>
2023-03-26 20:03:06 -0500

    Refactoring cli

--- ytbd.py
+++ ytbd.py
@@ -1,39 +1,46 @@
 
 import os, sys
 from pytube import YouTube
-import re
+# import rex
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-u','--url', action='extend', nargs='*', help='URL to download instrumental from')
+parser.add_argument('-a','--analyze', action='extend', nargs='*')
 
-ENDPOINT = 'https://www.youtube.com/watch?v=382mKYYNhY0&list=PL2ntw5yLXvVUoUlbNRb4dkL-EwTtycPFD&index=24' #your enpoint here
 
-def do_download(endpoint):
-    try:
 
-        yt = YouTube(endpoint)
-        dest = yt.streams[0].title + '.mp3' 
-        dest = dest.replace('\"', '')
-        dest = dest.replace('.mp4','')
-        dest = dest.replace('*', '')
+args = parser.parse_args()
 
-        video = yt.streams.filter(only_audio=True).first()
+# print(args.url)
+
+# ENDPOINT = 'https://www.youtube.com/watch?v=382mKYYNhY0&list=PL2ntw5yLXvVUoUlbNRb4dkL-EwTtycPFD&index=24' #your enpoint here
+
+# def do_download(endpoint):
+#     try:
+
+#         yt = YouTube(endpoint)
+#         dest = yt.streams[0].title + '.mp3' 
+#         dest = dest.replace('\"', '')
+#         dest = dest.replace('.mp4','')
+#         dest = dest.replace('*', '')
+
+#         video = yt.streams.filter(only_audio=True).first()
         
-        outfile = video.download()
+#         outfile = video.download()
 
-        if not os.path.exists(dest):
-            os.rename(outfile,dest)
-        else:
-            print('Cannot copy to that location, already exists!')
-    except Exception as e:
-        print(str(e))
+#         if not os.path.exists(dest):
+#             os.rename(outfile,dest)
+#         else:
+#             print('Cannot copy to that location, already exists!')
+#     except Exception as e:
+#         print(str(e))
 
     
-def main(argv):
+def main():
 
-    print(argv)
-    # do_download(ENDPOINT)
-    for enpoint in argv:
-        do_download(enpoint)
+   pass
 
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
